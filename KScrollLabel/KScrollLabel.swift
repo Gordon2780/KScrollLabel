d3f640b052e761778c5f6e35c1c8da0ac98019f8
秦伟宽 <qinweikuan@inke.cn>
2021-10-13 19:03:12 +0800

    1.增加支持滚动字幕首尾间距修改 2.增加文字阴影字体等属性

--- KScrollLabel/KScrollLabel.swift
+++ KScrollLabel/KScrollLabel.swift
@@ -9,14 +9,18 @@ import UIKit
 
 class KScrollLabel: UIView {
     
+    /// 滚动样式，默认滚动到结尾从头开始
+    var style: ScrollDirection.Style = .reset
     /// 滚动中的方向
     private(set) var scrolling: ScrollDirection = .left
-    /// 显示文本的控件
-    private(set) var titleLabel: UILabel!
+    /// 滚动内容大小
+    private(set) var contentSize: CGSize = .zero
     /// 滚动方向 默认水平
-    var direction: UICollectionView.ScrollDirection = .horizontal{
+    @IBInspectable var direction: UICollectionView.ScrollDirection = .horizontal{
         didSet{ setString(text) }
     }
+    /// 首尾间距
+    @IBInspectable var tailSpace: CGFloat = 20
     /// 滚动频率/秒
     @IBInspectable var duration: CGFloat = 0.25
     /// 屏幕刷新率 默认60hz
@@ -25,9 +29,28 @@ class KScrollLabel: UIView {
     @IBInspectable var text: String? {
         didSet{ setString(oldValue) }
     }
-    /// 滚动内容大小
-    @IBInspectable var contentSize: CGSize = .zero {
-        didSet{ setScrollContentSize() }
+    /// 字体
+    @IBInspectable var font: UIFont = .systemFont(ofSize: 14) {
+        didSet{
+            firstLabel.font = font
+            lastLabel.font = font
+        }
+    }
+    /// 文字颜色
+    @IBInspectable var textColor: UIColor = .black {
+        didSet{
+            firstLabel.textColor = textColor
+            lastLabel.textColor = textColor
+        }
+    }
+    
+    
+    /// 阴影颜色
+    @IBInspectable var shadowColor: UIColor? {
+        didSet {
+            firstLabel.shadowColor = shadowColor
+            lastLabel.shadowColor = shadowColor
+        }
     }
     /// 内边距
     @IBInspectable var insets: UIEdgeInsets = .zero{
@@ -39,6 +62,11 @@ class KScrollLabel: UIView {
     private var maxOffset: CGFloat = 0
     private var minOffset: CGFloat = 0
     private var scrollView: UIScrollView!
+    private var lastLabel: UILabel!
+    private var firstLabel: UILabel!
+    private var displayLabel: UILabel?
+    private var contentSizeIsEmpty: Bool { contentSize.width == 0 || contentSize.height == 0}
+
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -51,8 +79,22 @@ class KScrollLabel: UIView {
     }
 
     override func layoutSubviews() {
-       titleLabel.frame = CGRect(x: 0, y: 0, width: contentSize.width , height:  contentSize.height)
+        
+        if contentSizeIsEmpty {
+            setContentSize()
+        }
+        
+        firstLabel.frame = CGRect(x: 0, y: 0, width: contentSize.width , height:  contentSize.height)
+        if  direction == .horizontal {
+            lastLabel.frame = CGRect(x: firstLabel.frame.maxX + tailSpace, y: 0, width: contentSize.width, height: contentSize.height)
+        }else if direction == .vertical {
+            lastLabel.frame = CGRect(x: 0, y: firstLabel.frame.maxY + tailSpace, width: contentSize.width, height: contentSize.height)
+        }
        scrollView.frame = CGRect(x: insets.left, y: insets.top, width: bounds.width - insets.left - insets.right, height: bounds.height - insets.top - insets.bottom)
+       
+        if !contentSizeIsEmpty && displayLink == nil {
+            startScrollable()
+        }
    }
     
 }
@@ -60,6 +102,18 @@ class KScrollLabel: UIView {
 fileprivate extension KScrollLabel {
     
     func createViews(){
+        
+        func defaultLabel(tag: Int) -> UILabel{
+            let label = UILabel()
+            label.tag = tag
+            label.numberOfLines = 0
+            label.font = font
+            label.textColor = textColor
+            label.textAlignment = .left
+            return label
+        }
+        
+        
         let lay = UICollectionViewFlowLayout()
         lay.scrollDirection = .horizontal
         scrollView = UIScrollView()
@@ -69,67 +123,89 @@ fileprivate extension KScrollLabel {
         scrollView.isUserInteractionEnabled = false
         addSubview(scrollView)
         
-        titleLabel = UILabel()
-        titleLabel.numberOfLines = 0
-        titleLabel.font = UIFont.systemFont(ofSize: 15)
-        titleLabel.textColor = .black
-        titleLabel.textAlignment = .left
-        scrollView.addSubview(titleLabel)
+        firstLabel = defaultLabel(tag: 1)
+        scrollView.addSubview(firstLabel)
+        
+        lastLabel = defaultLabel(tag: 2)
+        scrollView.addSubview(lastLabel)
+        
+        displayLabel = firstLabel
+        
     }
     
     func setString(_ oldValue: String?){
         
         guard let string = text else { return }
-        titleLabel.text = string
-        if contentSize == .zero || (contentSize != .zero && string != oldValue){
-            let font = titleLabel.font ?? .systemFont(ofSize: 15)
-            var maxSize: CGSize = .zero
-            if  direction == .horizontal {
-                maxSize = CGSize(width: CGFloat.greatestFiniteMagnitude, height: bounds.height-insets.top-insets.bottom)
-            }else if direction == .vertical{
-                maxSize = CGSize(width: bounds.width-insets.left-insets.right, height: CGFloat.greatestFiniteMagnitude)
-            }
-            let stringSize = string.boundingRect(with: maxSize, options: [.usesLineFragmentOrigin,.usesFontLeading], attributes: [.font:font], context: nil).size
-            
-            if direction == .horizontal {
-                contentSize = CGSize(width: stringSize.width, height: maxSize.height)
-            }else if direction == .vertical{
-                contentSize = CGSize(width: maxSize.width, height: stringSize.height)
-            }
-            
-           
-        }else{
-           setScrollContentSize()
+        firstLabel.text = string
+        lastLabel.text = string
+        if string != oldValue {
+            setContentSize()
+        }
+        if frame != .zero {
+            setNeedsLayout()
+            startScrollable()
         }
     }
     
-    func setScrollContentSize(){
-        setNeedsLayout()
-        scrollView.contentSize = contentSize
+    func setContentSize(){
         
+        guard let string = firstLabel.text else { return  }
+        
+        var maxSize: CGSize = .zero
+        if  direction == .horizontal {
+            maxSize = CGSize(width: CGFloat.greatestFiniteMagnitude, height: bounds.height-insets.top-insets.bottom)
+        }else if direction == .vertical{
+            maxSize = CGSize(width: bounds.width-insets.left-insets.right, height: CGFloat.greatestFiniteMagnitude)
+        }
+        
+        let stringSize = string.boundingRect(with: maxSize, options: [.usesLineFragmentOrigin,.usesFontLeading], attributes: [.font : font], context: nil).size
+        
+        if direction == .horizontal {
+            contentSize = CGSize(width: stringSize.width, height: maxSize.height)
+        }else if direction == .vertical {
+            contentSize = CGSize(width: maxSize.width, height: stringSize.height)
+        }
+        scrollView.contentSize = contentSize
+    }
+    
+    
+    func startScrollable(){
+        print("startScrollable")
         var scrollable: Bool = false
         
         if direction == .horizontal {
             rollOffset = contentSize.width / (hertz * (duration * 60))
-            maxOffset = contentSize.width - bounds.width
-            scrolling = (scrollView.contentOffset.x < maxOffset) ? .right:.left
+            if style == .reset {
+                maxOffset = contentSize.width + scrollView.bounds.width + tailSpace
+            }else if style == .reverse {
+                maxOffset = contentSize.width - bounds.width
+                scrolling = (scrollView.contentOffset.x < maxOffset) ? .right:.left
+            }
             scrollable = (contentSize.width > bounds.width &&
                           text?.isEmpty == false &&
                           displayLink == nil)
         }else if direction == .vertical {
-            rollOffset = contentSize.height / (hertz * (duration * 60))
-            maxOffset = contentSize.height - bounds.height
-            scrolling = (scrollView.contentOffset.y < maxOffset) ? .top:.bottom
+            rollOffset = (contentSize.height + tailSpace) / (hertz * (duration * 60))
+            if style == .reverse {
+                maxOffset = contentSize.height - bounds.height
+                scrolling = (scrollView.contentOffset.y < maxOffset) ? .top:.bottom
+            }else if style == .reset {
+                maxOffset = contentSize.height + tailSpace
+            }
             scrollable = (contentSize.height > bounds.height &&
                           text?.isEmpty == false &&
                           displayLink == nil)
         }
         
         if scrollable{
+            invalidate()
+            lastLabel.isHidden = false
             displayLink = CADisplayLink(target: self, selector: #selector(displayLinkAction))
-            displayLink?.add(to: RunLoop.current, forMode: .default)
+            displayLink?.add(to: .current, forMode: .default)
             displayLink?.isPaused = false
+            
         }else{
+            lastLabel.isHidden = true
             invalidate()
         }
     }
@@ -142,19 +218,45 @@ fileprivate extension KScrollLabel {
         }
     }
     
-    func setScrolling(){
+    func reverseStyleScroll(){
         displayLink?.isPaused = true
-        switch scrolling {
-        case .left:
-            scrolling = .right
-        case .right:
-            scrolling = .left
-        case .top:
-            scrolling = .bottom
-        case .bottom:
-            scrolling = .top
-        }
-        displayLink?.isPaused = false
+            switch scrolling {
+            case .left:
+                scrolling = .right
+            case .right:
+                scrolling = .left
+            case .top:
+                scrolling = .bottom
+            case .bottom:
+                scrolling = .top
+            }
+       displayLink?.isPaused = false
+    }
+    
+    func tailedSpaceScroll(_ offset: CGFloat){
+        
+        guard let display = displayLabel else { return  }
+        
+            let value = direction == .horizontal ?  display.frame.maxX:display.frame.maxY
+            if  offset > value {
+                displayLink?.isPaused = true
+                if display.tag == 1 {
+                    if direction == .horizontal {
+                        firstLabel.frame.origin.x = lastLabel.frame.maxX + tailSpace
+                    }else if direction == .vertical {
+                        firstLabel.frame.origin.y = lastLabel.frame.maxY + tailSpace
+                    }
+                    displayLabel = lastLabel
+                }else if display.tag == 2 {
+                    if direction == .horizontal {
+                        lastLabel.frame.origin.x = firstLabel.frame.maxX + tailSpace
+                    }else if direction == .vertical {
+                        lastLabel.frame.origin.y = firstLabel.frame.maxY + tailSpace
+                    }
+                    displayLabel = firstLabel
+                }
+                displayLink?.isPaused = false
+            }
     }
     
     func invalidate(){
@@ -171,19 +273,22 @@ fileprivate extension KScrollLabel {
         }else{
             let offset = (direction == .horizontal) ? scrollView.contentOffset.x:scrollView.contentOffset.y
             
-            switch scrolling {
-            case .right,.bottom:
-                
-                if offset >= maxOffset {
-                    setScrolling()
-                }
+            if style == .reset {
+                tailedSpaceScroll(offset)
                 setOffset(offset+rollOffset)
-            case .left,.top:
-                
-                if offset <= minOffset{
-                    setScrolling()
+            }else if style == .reverse {
+                switch scrolling {
+                case .right,.bottom:
+                        if offset >= maxOffset {
+                            reverseStyleScroll()
+                        }
+                    setOffset(offset+rollOffset)
+                case .left,.top:
+                        if offset <= minOffset{
+                            reverseStyleScroll()
+                        }
+                    setOffset(offset-rollOffset)
                 }
-                setOffset(offset-rollOffset)
             }
         }
     }
@@ -195,6 +300,11 @@ extension KScrollLabel {
         case right
         case top
         case bottom
+        
+        enum Style {
+        case reset
+        case reverse
+        }
     }
 }
 
--- KScrollLabel/ViewController.swift
+++ KScrollLabel/ViewController.swift
@@ -18,28 +18,43 @@ class ViewController: UIViewController {
         label.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(clickItem)))
         
         
-        let label1 = KScrollLabel(frame: CGRect(x: 20, y: 200, width: UIScreen.main.bounds.width-40, height: 40))
-        label1.titleLabel.textColor = .white
+        let label1 = KScrollLabel()
+        label1.frame = CGRect(x: 20, y: 200, width: UIScreen.main.bounds.width-40, height: 40)
+        label1.textColor = .white
+        label.tailSpace = 30
         label1.backgroundColor = .purple
-        label1.duration = 0.5
-        label1.text = "无代码污染，无入侵项目,轻量级可滚动label，使用简单方便，支持pod，CADisplayLink实现。无代码污染，无入侵项目,轻量级可滚动label，使用简单方便，支持pod，CADisplayLink实现。无代码污染，无入侵项目,轻量级可滚动label，使用简单方便，支持pod，CADisplayLink实现。"
+        label1.duration = 0.1
+        label1.shadowColor = UIColor.magenta
+        label1.text = "无代码污染，"
+        
         view.addSubview(label1)
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+            label1.text =  "无入侵项目,无代码污染，无入侵项目无代码污染，无入侵项目无代码污染，无入侵项目无代码污染，无入侵项目无代码污染，无入侵项目无代码污染，无入侵项目"
+        }
 
+        
         let label2 = KScrollLabel(frame: CGRect(x: 40, y: 300, width: 15, height: UIScreen.main.bounds.width-40))
         label2.backgroundColor = .orange
+        label2.style = .reverse
+        label2.tailSpace =  50
         label2.direction = .vertical
         label2.duration = 0.8
+        label2.textColor = .red
         label2.text = "无代码污染，无入侵项目,轻量级可滚动label，CADisplayLink实现。我是滚动内容，我是滚动内容，我是滚动内容，我是滚动内容，我是滚动内容，我是滚动内容，我是滚动内容，我是滚动内容，我是滚动内容，我是滚动内容，"
         view.addSubview(label2)
-        
-        
+
+//
         let label3 = KScrollLabel(frame: CGRect(x: 140, y: 300, width: 40, height: UIScreen.main.bounds.width-40))
         label3.backgroundColor = .green
         label3.direction = .vertical
+        label3.tailSpace = 100
         label3.duration = 0.2
+        label3.tailSpace = 15
+        label3.textColor = .white
+
         label3.text = "无代码污染，无入侵项目,轻量级可滚动label，CADisplayLink实现。我是滚动内容，我是滚动内容，我是滚动内容，我是滚动内容，我是滚动内容，我是滚动内容，我是滚动内容，我是滚动内容，我是滚动内容，我是滚动内容，"
         view.addSubview(label3)
-        
         
     }
 
